181dd3e3218979328a284ea8d9212486793208fa
Nick Ferritto <Nick.Ferritto@Neudesic.com>
2023-03-24 11:18:02 -0400

    Working post recommendations request

--- src/api/Controllers/BandsController.cs
+++ src/api/Controllers/BandsController.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Linq;
 using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Reflection;
 using RestSharp;
 using api.Models;
 using System.Threading.Tasks;
@@ -26,11 +21,103 @@ namespace api.Controllers
         }
 
         [HttpPost]
-        public IActionResult PostRecommendations()
+        public async Task<IActionResult> PostRecommendations(PostRecommendBandRequest requestIn)
         {
-            return Ok("hello world ");
-            //var Team = team;
-            //return team.teamMembers.Any() ? Ok(Team) : NotFound();
+            string messageLikes = "";
+            string messageDislikes = "";
+            string messageUsedRecommendations = "";
+
+
+            if (requestIn.Likes.Count == 1)
+            {
+                messageLikes = $"I like the band {requestIn.Likes[0]}. ";
+            }
+            else if (requestIn.Likes.Count > 1)
+            {
+                messageLikes = "I like the bands ";
+                for (int i = 0; i < requestIn.Likes.Count; i++)
+                {
+                    if (i < requestIn.Likes.Count - 1)
+                    {
+                        messageLikes += $"{requestIn.Likes[i]}, ";
+                    }
+                    else
+                    {
+                        messageLikes += $"and {requestIn.Likes[i]}. ";
+                    }
+                }
+            }
+
+            if (requestIn.Dislikes.Count == 1)
+            {
+                messageDislikes = $"I dislike the band {requestIn.Dislikes[0]}. ";
+            }
+            else if (requestIn.Dislikes.Count > 1)
+            {
+                messageDislikes = "I dislike the bands ";
+                for (int i = 0; i < requestIn.Dislikes.Count; i++)
+                {
+                    if (i < requestIn.Dislikes.Count - 1)
+                    {
+                        messageDislikes += $"{requestIn.Dislikes[i]}, ";
+                    }
+                    else
+                    {
+                        messageDislikes += $"and {requestIn.Dislikes[i]}. ";
+                    }
+                }
+            }
+
+            if (requestIn.Recommendations.Count == 1)
+            {
+                messageUsedRecommendations = $"Do not include the band {requestIn.Dislikes[0]} in your recommendations. ";
+            }
+            else if (requestIn.Recommendations.Count > 1)
+            {
+                messageUsedRecommendations = "Do not include the bands ";
+                for (int i = 0; i < requestIn.Recommendations.Count; i++)
+                {
+                    if (i < requestIn.Recommendations.Count - 1)
+                    {
+                        messageUsedRecommendations += $"{requestIn.Recommendations[i]}, ";
+                    }
+                    else
+                    {
+                        messageUsedRecommendations += $"and {requestIn.Recommendations[i]} in your recommendations. ";
+                    }
+                }
+            }
+
+            string messageEnd = "What bands would you recommend listening to? Format as json with the fields likes, dislikes, and recommendations";
+
+
+            string finalMessage = messageLikes + messageDislikes + messageUsedRecommendations + messageEnd;
+            // MessagesObject message = new MessagesObject("user", "I like the bands Britney Spears, and christina aguilera.   What bands would you recommend listening to?  Format as json with the fields likes, dislikes, and recommendations");
+
+
+            Uri baseUrl = new Uri("https://api.openai.com/v1/chat");
+            IRestClient client = new RestClient(baseUrl);
+            RestRequest request = new RestRequest("completions", Method.Post);
+
+            string authToken = Configuration["Test_String"];
+
+            request.AddHeader("Authorization", $"Bearer {authToken}");
+            request.AddHeader("Content-Type", "application/json");
+
+            MessagesObject message = new MessagesObject("user", finalMessage);
+
+
+            OpenAiPostRequest requestBody = new OpenAiPostRequest("gpt-3.5-turbo", new List<MessagesObject> { message });
+
+            request.AddBody(requestBody);
+
+
+            RestResponse<OpenAiPostResponse> response = await client.ExecuteAsync<OpenAiPostResponse>(request);
+
+
+            OpenAiPostResponse res = JsonSerializer.Deserialize<OpenAiPostResponse>(response.Content);
+
+            return Ok(res.choices[0].message.content);
         }
 
         [HttpGet]
@@ -52,132 +139,13 @@ namespace api.Controllers
 
             request.AddBody(requestBody);
 
-            
+
             RestResponse<OpenAiPostResponse> response = await client.ExecuteAsync<OpenAiPostResponse>(request);
 
-            
+
             OpenAiPostResponse res = JsonSerializer.Deserialize<OpenAiPostResponse>(response.Content);
 
             return Ok(res.choices[0].message.content);
-
-            //    namespace RestSharpThingy
-            //{
-            //    using System;
-            //    using System.Collections.Generic;
-            //    using System.IO;
-            //    using System.Linq;
-            //    using System.Net;
-            //    using System.Reflection;
-            //    using RestSharp;
-
-            //    public static class Program
-            //    {
-            //        public static void Main()
-            //        {
-            //            Uri baseUrl = new Uri("https://httpbin.org/");
-            //            IRestClient client = new RestClient(baseUrl);
-            //            IRestRequest request = new RestRequest("get", Method.GET) { Credentials = new NetworkCredential("testUser", "P455w0rd") };
-
-            //            request.AddHeader("Authorization", "Bearer qaPmk9Vw8o7r7UOiX-3b-8Z_6r3w0Iu2pecwJ3x7CngjPp2fN3c61Q_5VU3y0rc-vPpkTKuaOI2eRs3bMyA5ucKKzY1thMFoM0wjnReEYeMGyq3JfZ-OIko1if3NmIj79ZSpNotLL2734ts2jGBjw8-uUgKet7jQAaq-qf5aIDwzUo0bnGosEj_UkFxiJKXPPlF2L4iNJSlBqRYrhw08RK1SzB4tf18Airb80WVy1Kewx2NGq5zCC-SCzvJW-mlOtjIDBAQ5intqaRkwRaSyjJ_MagxJF_CLc4BNUYC3hC2ejQDoTE6HYMWMcg0mbyWghMFpOw3gqyfAGjr6LPJcIly__aJ5__iyt-BTkOnMpDAZLTjzx4qDHMPWeND-TlzKWXjVb5yMv5Q6Jg6UmETWbuxyTdvGTJFzanUg1HWzPr7gSs6GLEv9VDTMiC8a5sNcGyLcHBIJo8mErrZrIssHvbT8ZUPWtyJaujKvdgazqsrad9CO3iRsZWQJ3lpvdQwucCsyjoRVoj_mXYhz3JK3wfOjLff16Gy1NLbj4gmOhBBRb8rJnUXnP7rBHs00FAk59BIpKLIPIyMgYBApDCut8V55AgXtGs4MgFFiJKbuaKxq8cdMYEVBTzDJ-S1IR5d6eiTGusD5aFlUkAs9NV_nFw");
-            //            request.AddParameter("clientId", 123);
-
-            //            IRestResponse<RootObject> response = client.Execute<RootObject>(request);
-
-            //            if (response.IsSuccessful)
-            //            {
-            //                response.Data.Write();
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine(response.ErrorMessage);
-            //            }
-
-            //            Console.WriteLine();
-
-            //            string path = Assembly.GetExecutingAssembly().Location;
-            //            string name = Path.GetFileName(path);
-
-            //            request = new RestRequest("post", Method.POST);
-            //            request.AddFile(name, File.ReadAllBytes(path), name, "application/octet-stream");
-            //            response = client.Execute<RootObject>(request);
-            //            if (response.IsSuccessful)
-            //            {
-            //                response.Data.Write();
-            //            }
-            //            else
-            //            {
-            //                Console.WriteLine(response.ErrorMessage);
-            //            }
-
-            //            Console.ReadLine();
-            //        }
-
-            //        private static void Write(this RootObject rootObject)
-            //        {
-            //            Console.WriteLine("clientId: " + rootObject.args.clientId);
-            //            Console.WriteLine("Accept: " + rootObject.headers.Accept);
-            //            Console.WriteLine("AcceptEncoding: " + rootObject.headers.AcceptEncoding);
-            //            Console.WriteLine("AcceptLanguage: " + rootObject.headers.AcceptLanguage);
-            //            Console.WriteLine("Authorization: " + rootObject.headers.Authorization);
-            //            Console.WriteLine("Connection: " + rootObject.headers.Connection);
-            //            Console.WriteLine("Dnt: " + rootObject.headers.Dnt);
-            //            Console.WriteLine("Host: " + rootObject.headers.Host);
-            //            Console.WriteLine("Origin: " + rootObject.headers.Origin);
-            //            Console.WriteLine("Referer: " + rootObject.headers.Referer);
-            //            Console.WriteLine("UserAgent: " + rootObject.headers.UserAgent);
-            //            Console.WriteLine("origin: " + rootObject.origin);
-            //            Console.WriteLine("url: " + rootObject.url);
-            //            Console.WriteLine("data: " + rootObject.data);
-            //            Console.WriteLine("files: ");
-            //            foreach (KeyValuePair<string, string> kvp in rootObject.files ?? Enumerable.Empty<KeyValuePair<string, string>>())
-            //            {
-            //                Console.WriteLine("\t" + kvp.Key + ": " + kvp.Value);
-            //            }
-            //        }
-            //    }
-
-            //    public class Args
-            //    {
-            //        public string ClientId { get; set; }
-            //    }
-
-            //    public class Headers
-            //    {
-            //        public string Accept { get; set; }
-
-            //        public string AcceptEncoding { get; set; }
-
-            //        public string AcceptLanguage { get; set; }
-
-            //        public string Authorization { get; set; }
-
-            //        public string Connection { get; set; }
-
-            //        public string Dnt { get; set; }
-
-            //        public string Host { get; set; }
-
-            //        public string Origin { get; set; }
-
-            //        public string Referer { get; set; }
-
-            //        public string UserAgent { get; set; }
-            //    }
-
-            //    public class RootObject
-            //    {
-            //        public Args args { get; set; }
-
-            //        public Headers Headers { get; set; }
-
-            //        public string Origin { get; set; }
-
-            //        public string Url { get; set; }
-
-            //        public string Data { get; set; }
-            //        public Dictionary<string, string> Files { get; set; }
-            //    }
-            //}
 
         }
 
